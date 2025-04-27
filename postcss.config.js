dc556e5744854dda4718f1bba4066e996f315c6d
Gorman <838584740@qq.com>
2025-04-27 21:21:49 +0800

    test

--- postcss.config.js
+++ postcss.config.js
@@ -1,16 +1,16 @@
 // postcss.config.js
 module.exports = {
   plugins: {
-    "postcss-preset-env": {
+    'postcss-preset-env': {
       stage: 3,
       features: {
-        "nesting-rules": true,
+        'nesting-rules': true,
       },
       autoprefixer: {
-        flexbox: "no-2009",
+        flexbox: 'no-2009',
         grid: true,
       },
     },
-    ...(process.env.NODE_ENV === "production" ? { cssnano: {} } : {}),
+    ...(process.env.NODE_ENV === 'production' ? { cssnano: {} } : {}),
   },
 };
